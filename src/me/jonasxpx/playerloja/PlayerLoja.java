8b1f3bb327e86061d55fad615e80d7ae03435616
JonasXPX <jonasdefarias@hotmail.com>
2015-05-31 18:36:01 -0300

    Adicionado metrics

--- src/me/jonasxpx/playerloja/PlayerLoja.java
+++ src/me/jonasxpx/playerloja/PlayerLoja.java
@@ -1,7 +1,11 @@
 package me.jonasxpx.playerloja;
 
+import java.io.IOException;
+import java.util.logging.Level;
+
 import me.jonasxpx.playerloja.commands.DelLoja;
 import me.jonasxpx.playerloja.commands.SetLoja;
+import me.mcstats.Metrics;
 
 import org.bukkit.plugin.java.JavaPlugin;
 /**
@@ -19,6 +23,12 @@ public class PlayerLoja extends JavaPlugin {
 		getCommand("delloja").setExecutor(new DelLoja(this));
 		getCommand("setloja").setExecutor(new SetLoja(this));
 		getCommand("loja").setExecutor(new me.jonasxpx.playerloja.commands.Loja(this));
+		try{
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		}catch(IOException ex){
+			getLogger().log(Level.WARNING, "Não foi possivel conectar a Metrics");
+		}
 	}
 
 	public final static PlayerLoja getThisPlugin(){
